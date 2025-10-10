7f9617b75e6456fe1b00517b2b2ccbbb0f1d7701
Gabriel SC <admgabriel512@gmail.com>
2025-10-10 00:55:29 -0300

    BackEnd configuração
    foi criado a pasta do backend para fazer a api e depois ela foi configurada

---
+++ backend/src/main/java/com/gasparelli/doolt/DooltApplication.java
@@ -0,0 +1,13 @@
+package com.gasparelli.doolt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DooltApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DooltApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/gasparelli/doolt/DooltApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gasparelli.doolt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DooltApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
